7769229d03fe779ee7313cfb56e7fb8418630ab2
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-08-02 20:17:38 +0300

    configure watch and livereload workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,35 @@ module.exports = function(grunt) {
       dist: {
         options: {
           sassDir: 'src/sass',
-          cssDir: 'src/css'
+          cssDir: 'src/css',
+          imagesDir: 'src/img'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: 'src/sass/**/*',
+        tasks: 'compass',
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          livereload: true,
+          base: 'src'
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task(s).
   grunt.registerTask('default', ['compass']);
